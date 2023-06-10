9dc08b13f20bf1a5a317f38ce9a9c9c83905652d
Winston Fassett <winston.fassett@gmail.com>
2023-06-10 09:40:42 -0500

    yo langium

---
+++ src/language/statetree-validator.ts
@@ -0,0 +1,31 @@
+import { ValidationAcceptor, ValidationChecks } from 'langium';
+import { StatetreeAstType, Person } from './generated/ast';
+import type { StatetreeServices } from './statetree-module';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: StatetreeServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.StatetreeValidator;
+    const checks: ValidationChecks<StatetreeAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class StatetreeValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
