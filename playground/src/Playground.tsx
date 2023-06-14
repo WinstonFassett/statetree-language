c4fac81c3f14285e5a4e77bbcab76fbe6e52fd43
Winston Fassett <winston.fassett@gmail.com>
2023-06-14 18:49:29 -0500

    lazy/suspense

--- playground/src/Playground.tsx
+++ playground/src/Playground.tsx
@@ -1,9 +1,10 @@
-// import Editor from '@monaco-editor/react';
-
-import { useState } from "react";
-import { Editor } from "./Editor";
+import { useState, lazy, Suspense } from "react";
 import { State, Statemachine } from "../../src/language/generated/ast";
 import { Visualization } from "./Visualization";
+
+const Editor = lazy(
+  () => import('./Editor').then(module => ({ default: module.Editor }))
+);
 
 export function Playground() {
   const [model, setModel] = useState<Statemachine>()
@@ -16,7 +17,9 @@ export function Playground() {
       /> */}
       <div style={{ display: 'flex', alignItems: 'stretch', justifyContent: 'stretch' }}>
         <div style={{ flex: 1, paddingTop: 10 }}>
-          <Editor onModelCreated={(model) => { setModel(model) }} />
+          <Suspense fallback={<div>Loading editor...</div>}>
+            <Editor onModelCreated={(model) => { setModel(model) }} />
+          </Suspense>          
         </div>
         <div style={{ flex: 1 }}>
           {!!model && <Visualization model={model} />}
