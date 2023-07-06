eb8ff08d2e1ed65968f4235b6a3afae1a4916249
WinstonFassett <winston.fassett@clickfunnels.com>
2023-07-06 17:09:58 -0400

    mods to revert

--- src/language/statetree-module.ts
+++ src/language/statetree-module.ts
@@ -5,8 +5,6 @@ import {
 } from 'langium';
 import { StatetreeGeneratedModule, StatetreeGeneratedSharedModule } from './generated/module';
 import { StatetreeValidator, registerValidationChecks } from './statetree-validator';
-import { StatetreeFormatter } from './statetree-formatter';
-import { StatetreeScopeComputation } from './statetree-scope';
 
 /**
  * Declaration of custom services - add your own service classes here.
@@ -29,17 +27,9 @@ export type StatetreeServices = LangiumServices & StatetreeAddedServices
  * selected services, while the custom services must be fully specified.
  */
 export const StatetreeModule: Module<StatetreeServices, PartialLangiumServices & StatetreeAddedServices> = {
-    references: {
-        ScopeComputation: (services) => new StatetreeScopeComputation(services),
-        QualifiedNameProvider: () => new QualifiedNameProvider()
-    },
     validation: {
         StatetreeValidator: () => new StatetreeValidator()
-    },
-    lsp: {
-        Formatter: () => new StatetreeFormatter(),
-        RenameProvider: (services) => new StatetreeRenameProvider(services)
-    }    
+    }
 };
 
 /**
