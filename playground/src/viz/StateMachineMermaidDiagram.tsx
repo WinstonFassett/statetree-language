774657b52eef180e45bd50defb65cb2d00ce8145
Winston Fassett <winston.fassett@gmail.com>
2023-07-07 22:48:34 -0500

    progress, but doesn't work with checkout example right now

--- playground/src/viz/StateMachineMermaidDiagram.tsx
+++ playground/src/viz/StateMachineMermaidDiagram.tsx
@@ -12,11 +12,10 @@ mermaid.initialize({
 const Mermaid = ({ chart }: any) => {
   let node: HTMLDivElement|null;
   useEffect(() => {
-    // console.log('rendering', chart)
+    console.log('rendering', chart)
     mermaid.contentLoaded();
-    node.removeAttribute("data-processed");
-    // console.log('rendered')
     node?.removeAttribute("data-processed");
+    console.log('rendered')
   }, [chart]);
   if (!chart) return null;
   return (
@@ -27,8 +26,13 @@ const Mermaid = ({ chart }: any) => {
 };
 
 function toStateDiagram(definition: Statemachine, machine: StateMachineInstance, callbackName: string) {
+  console.log('statediagram', { definition, machine })
   const rows = [] as any[];
   let indents = 0
+
+  // at each level,
+  // render transitions, then composite states
+  const renderedStates = new Set()
   addStates(definition.states)
   const example = `
 stateDiagram-v2
@@ -47,26 +51,36 @@ stateDiagram-v2
 `;
 const diagram = `
 stateDiagram-v2
+%%    direction LR
 ${rows.join('\n')}
 `
 // classDef active fill:aquamarine;
   // console.log(diagram)
-  return example;
+  return diagram;
   function indent() {
     return Array(indents*4).fill(' ').join('')
   }
+
   function addStates (states: State[]) {
+    const allStates = new Set()
+
     indents +=1
+    const compositeStates: State[] = []
+
+    // determine if transition is local
+    // handle non-local transitions
+    // add end-thingy, then named transition from main state to container with new state, etc
+
     states.forEach((state, index) => {
+      allStates.add(state)
       const key = state.name
       const isActive = key === machine.state?.name
       // const state = definition.states[key];
       // console.log("state", key, state);
       const ifactive = key === machine.state?.name ? ":::active" : "";
       // rows.push(["    ", key, ifactive].join(""));
-      rows.push(`${indent()}state ${state.name} {`)
+      let renderedState = false
       if (state.transitions) {
-        indents+=1
         state.transitions.forEach((transition) => {
           // console.log("transition", transition);
           const target = transition.to?.ref?.name;
@@ -74,21 +88,39 @@ ${rows.join('\n')}
             const event = transition.event
             // rows.push(`    ${key}-->|${event}| ${target}[${target}]`);
             rows.push(`${indent()}${key} --> ${target}: ${event}`);
+            renderedStates.add(state)
+            renderedStates.add(transition.to.ref)
+            renderedState = true
           }
         });
-        indents-=1
+      } else {
+        rows.push(`${indent()}${state.name} --> [*]`)
+        // need something here so the state appears?
+        // end node
       }
-      if (state.states) {
-        addStates(state.states)
-        // state.states.forEach(state => {
-        //   rows.push(`state ${state.name} {
-  
-        //   }`)
-        // })
+      if (!renderedState) {
+
       }
-      rows.push(`${indent()}}`)
+
+      // if composite
+      if (state.states && state.states.length>0) {
+        compositeStates.push(state)        
+      }
       // rows.push(["   ", "click", key, callbackName].join(" "));
     });
+
+    compositeStates.forEach(state => {
+      rows.push(`${indent()}state ${state.name} {`)
+      addStates(state.states)
+
+      rows.push(`${indent()}}`)
+    })
+
+    const unrenderedStates = states.filter(state => !renderedStates.has(state))
+    unrenderedStates.forEach(state => {
+      // rows.push(`${indent()}[*] --> ${state.name}`)
+    })
+
     indents -=1;
   }
 }
@@ -134,15 +166,20 @@ ${rows.join("\n")}
 }
 
 export default function StateMachineMermaidDiagram({ machine: instance }: { machine: StateMachineInstance}) {
-  //const [id] = useState(uuid.v4())
-  const id = "mermaidCallback";
-  (window as any)[id] = (...args: any[]) => {
-    console.log("callback", args);
-  };
-  const diagram = toStateDiagram(instance.model, instance, id);
+  const [id] = useState(uuid.v4())
+  const chart = useMemo(() => toStateDiagram(instance.model, instance, id), [instance, instance.model, id]);
+  if (!chart) return null;
+  console.log('chart', chart)
+  // const ref = useMermaid({ chart })
+  // return <div ref={ref}>{chart}</div>
+  // const id = "mermaidCallback";
+  // (window as any)[id] = (...args: any[]) => {
+  //   console.log("callback", args);
+  // };
+  
   return (
     <>
-      <Mermaid chart={diagram} />
+      <Mermaid chart={chart} />
       {/*<pre>{diagram}</pre>*/}
       {/*<pre>{JSON.stringify(definition, null, 2)}</pre>*/}
     </>
