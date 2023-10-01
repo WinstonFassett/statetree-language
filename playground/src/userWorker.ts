fc466c2d08aa6e6eb76ce7caae0ef6b21b6123a0
Winston Fassett <winston.fassett@gmail.com>
2023-10-01 10:02:28 -0500

    state tree worker

--- playground/src/userWorker.ts
+++ playground/src/userWorker.ts
@@ -3,26 +3,32 @@ import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
 import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import StatetreeWorker from '../libs/statetree-server-worker.js?worker'
 // import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution.js';
 // import 'monaco-editor/esm/vs/language/typescript/monaco.contribution.js';
 
+export function getWorker(_: any, label: string) {
+	if (label === 'json') {
+		return new jsonWorker();
+	}
+	if (label === 'css' || label === 'scss' || label === 'less') {
+		return new cssWorker();
+	}
+	if (label === 'html' || label === 'handlebars' || label === 'razor') {
+		return new htmlWorker();
+	}
+	if (label === 'typescript' || label === 'javascript') {
+		return new tsWorker();
+	}
+	if (label === 'statetree') {
+		return new StatetreeWorker()
+	}
+	return new editorWorker();
+}
+
 // @ts-ignore
 self.MonacoEnvironment = {
-	getWorker(_: any, label: string) {
-		if (label === 'json') {
-			return new jsonWorker();
-		}
-		if (label === 'css' || label === 'scss' || label === 'less') {
-			return new cssWorker();
-		}
-		if (label === 'html' || label === 'handlebars' || label === 'razor') {
-			return new htmlWorker();
-		}
-		if (label === 'typescript' || label === 'javascript') {
-			return new tsWorker();
-		}
-		return new editorWorker();
-	}
+	getWorker
 };
 
 // monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
