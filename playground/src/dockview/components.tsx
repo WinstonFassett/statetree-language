1ac7a985b84f02c9c090b6f82e840ae5933a09f7
Winston Fassett <winston.fassett@gmail.com>
2023-10-03 11:59:27 -0500

    hoisted

--- playground/src/dockview/components.tsx
+++ playground/src/dockview/components.tsx
@@ -4,7 +4,7 @@ import {
   IDockviewPanelHeaderProps
 } from 'dockview';
 
-import { SandpackPreview } from '@codesandbox/sandpack-react';
+import { SandpackPreview, useSandpack } from '@codesandbox/sandpack-react';
 import { SandpackMonacoEditor } from '../sandpack/SandpackMonacoEditor';
 import { StateMachinePane } from '../statetree-machine/StateMachinePane';
 import { DebugPane } from '../older/DebugPane';
@@ -12,6 +12,9 @@ import { StateMachineForceGraph } from '../viz/StateMachineForceGraphPane';
 import { StatetreeEditorPane } from '../sandpack/StateTreeEditorPane';
 import { StateMachineMermaidPane } from '../viz/StateMachineMermaidPane';
 import { StateMachinePlantUmlPane } from '../viz/StateMachinePlantUmlPane';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { SandpackPreviewPane } from '../sandpack/SandpackPreviewPane';
+import { HoistedDockviewPanel } from './HoistedDockViewPanel';
 
 export const components: PanelCollection<IDockviewPanelProps> = {
   default: (props: IDockviewPanelProps<{ someProps: string; }>) => {
@@ -42,9 +45,30 @@ export const components: PanelCollection<IDockviewPanelProps> = {
       <SandpackMonacoEditor afterEdit={props.params.afterEdit} filename={props.params.filename} language={props.params.language} />
     </div>;
   },
-  preview: (props: IDockviewPanelProps<{ someProps: string; }>) => {
-    return <SandpackPreview style={{ height: "100vh" }} />;
-  },
+  preview: HoistedDockviewPanel((props: IDockviewPanelProps<{ someProps: string; }>) => {
+    const dimensions = useRef({ height: 0, width: 0 })    
+    useEffect(() => {
+      console.log('first time')
+      const{dispose} = props.api.onDidDimensionsChange((event) => {
+        console.log('dimensions', { height: event.height, width: event.width });        
+        const { height, width } = event
+        dimensions.current = ({ height, width })
+      });      
+      // props.api.onDidDimensionsChange
+      // const disposable = props.api.onDidFocusChange(() => {
+      //     // write some code
+      // });
+      return () => {
+          dispose();
+      };
+    })    
+    // const { height, width } = dimensions.current
+    props.api.onDidVisibilityChange(e => {
+      console.log({visibility: e})
+    })
+    return <SandpackPreviewPane style={{ height: '400px' }}/>
+    
+  }),
 };
 export const headers: PanelCollection<IDockviewPanelHeaderProps> = {
   customTab: (props: IDockviewPanelHeaderProps) => {
